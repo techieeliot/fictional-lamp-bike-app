43f8e263c9019cefdc4411c34bf5e95ad7760262
Eliot Sanford <eliot587@gmail.com>
2020-06-08 07:59:57 -0500

    update: use conditional for dev and production build - add myApiClient to create baseURL call

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
-if (process.env.NODE_ENV != 'production') {
-  require('dotenv').config();
-}
 const express = require('express')
 const server = express();
 const cors = require('cors');
 server.use(cors());
+
+const process = require('process')
+const path = require('path')
+const index = (req, res) => {
+  if (process.env.NODE_ENV === 'development') {
+    require('dotenv').config();
+    res.status(200).redirect('http://localhost:3000')
+  } else {
+    res.sendFile(path.join(process.cwd(), 'build', 'index.html'))
+  }
+}
 
 const WB_API_KEY_A = process.env.WB_API_KEY_A
 const WB_API_KEY_B = process.env.WB_API_KEY_B
@@ -20,9 +28,9 @@ const getCoordinates = (req) => {
     const lat = req.query.lat
     const lon = req.query.lon
     console.log(lat, lon);
-     if(!lat) {
-       reject(lat);
-     } else resolve(lat && lon);
+    if(!lat) {
+      reject(lat);
+    } else resolve(lat && lon);
   });
 }
 
@@ -37,7 +45,12 @@ server.get('/', async (req, res, e) => {
   console.log(lat, lon)
   const API_WEEK_URL = `http://api.weatherbit.io/v2.0/forecast/daily?key=${WB_API_KEY_B}&days=7&units=I&lat=${lat}&lon=${lon}`
   const API_SEVERE_URL = `https://api.weatherbit.io/v2.0/alerts?lat=${lat}&lon=${lon}&key=${WB_API_KEY_B}`
-  await axios.get(API_WEEK_URL)
+  const myApiClient = axios.create({
+    baseURL: API_WEEK_URL,
+    timeout: 2000,
+    headers: {'X-Custom-Header': 'Weatherbit API'}
+  });
+  await myApiClient.get('/localforecast')
   .then(response => response.data)
   // .then(data => console.log(data))
   .then(data => res.send(data))
@@ -58,3 +71,5 @@ server.listen(PORT, err => {
   if (err) throw err
   console.log(`The Biker App server is listening on http://localhost:${PORT}!`)
 });
+
+module.exports = { index }
